2a9870515fe2bbe0bfa7ef5511d584da7062e1f3
Shubham Tewari <112378956+st321123@users.noreply.github.com>
2024-11-13 08:42:52 +0530

    Update details.tsx

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -2,7 +2,9 @@ import todoList from "./video/todoList.gif";
 import movieRecomend from "./video/movieRecomend.gif";
 import weather from "./video/weather.gif";
 import keeperApp from "./video/keeperApp.gif";
-const arr = [{ "img": weather, "link": "23","desc": "Weather Report","add":"https://weather-app-api-ten.vercel.app/" },
+const arr = [
+  { "img": BlogAndEarn, "link": "23", "desc": "Blog and Earn","add":"https://blog-and-earn-frontend.vercel.app" },
+ { "img": weather, "link": "23","desc": "Weather Report","add":"https://weather-app-api-ten.vercel.app/" },
  { "img": movieRecomend, "link": "23", "desc": "Movie Recommendation","add":"ADD" },
  { "img": keeperApp, "link": "23", "desc": "Keeper Web App","add":"https://new-keeper-app-nfo4.vercel.app/" },
  { "img": todoList, "link": "23", "desc": "Todo-List","add":"https://to-do-list-kohl-eight.vercel.app/" }];
